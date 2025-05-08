b1b60be2e908f3adadfe0ea04955ead3a4c49034
ctnalford <139728680+ctnalford@users.noreply.github.com>
2025-05-08 12:18:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 8080;
 app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
